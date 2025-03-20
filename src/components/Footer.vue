7eab0566533c7990909a883cbc710ff5ee65766e
LaCovLoid <tnsgus0018@naver.com>
2025-03-20 19:51:22 +0900

    footer @media쿼리 조정정

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,7 +17,8 @@
           Wales No. 10072356
           <br />
           <br />
-          Designed & developed by Bigger Picture
+          Designed & developed by
+          <spa style="text-decoration: underline">Bigger Picture</spa>
         </span>
       </div>
       <div class="center">
@@ -32,14 +33,16 @@
         </div>
       </div>
       <div class="right">
-        <span class="right-title"> Sign up to our newsletter </span>
-        <span class="right-subtitle">
-          Receive the latest mobile security news, exclusive discounts & offers
-          straight to your inbox!
-        </span>
-        <div class="input-container">
-          <input class="input-area" placeholder="Email address" />
-          <span class="submit-bt">submit</span>
+        <div class="yellow-container">
+          <span class="right-title"> Sign up to our newsletter </span>
+          <span class="right-subtitle">
+            Receive the latest mobile security news, exclusive discounts &
+            offers straight to your inbox!
+          </span>
+          <div class="input-container">
+            <input class="input-area" placeholder="Email address" />
+            <span class="submit-bt">submit</span>
+          </div>
         </div>
       </div>
     </div>
@@ -65,8 +68,8 @@
   margin: 0 auto;
   padding-top: 96px;
   padding-bottom: 96px;
-  grid-auto-flow: column; /* 1행 유지 */
-  gap: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
 
   background-color: #02033b;
   color: #ffffff;
@@ -80,23 +83,24 @@
   > .top {
     max-width: 1056px;
     margin: 0 auto;
-    display: grid;
-    grid-template-rows: 1fr; /* 1개의 행 */
-    grid-template-columns: repeat(3, 1fr); /* 3개의 열 */
-    align-items: flex-start;
+    display: flex;
+    justify-content: center;
 
     @include minimize() {
-      grid-template-rows: repeat(3, 1fr); /* 3개의 행 */
-      grid-template-columns: 1fr; /* 1개의 열 */
+      flex-direction: column;
     }
 
     > .left {
+      max-width: 312px;
+      width: 100%;
       font-size: 11px;
       font-weight: 200;
 
       @include minimize() {
-        width: 100%;
-        padding-left: 30px;
+        margin-top: 82px;
+        margin-left: 29px;
+        margin-right: 50px;
+
         order: 3;
       }
 
@@ -105,14 +109,26 @@
         font-weight: 700;
       }
       > .sns-list {
-        margin: 50px 0;
+        margin-top: 50px;
+        margin-bottom: 50px;
+
+        @include minimize() {
+          margin-top: 21px;
+          margin-bottom: 22px;
+        }
+
         > .sns-icon {
           width: 36px;
           margin-right: 40px;
         }
       }
       > .url-container {
-        margin: 0 50px 50px 0;
+        margin-right: 50px;
+        margin-bottom: 50px;
+
+        @include minimize() {
+          margin-bottom: 23px;
+        }
 
         > .url-text {
           margin-right: 40px;
@@ -122,12 +138,18 @@
     }
 
     > .center {
-      margin-left: 50px;
-      margin-right: 50px;
+      max-width: 188px;
+      width: 100%;
+      margin-left: 60px;
+      margin-right: 60px;
 
       @include minimize() {
-        width: 100%;
-        margin: 0;
+        max-width: 339px;
+        margin-top: 32px;
+        margin-left: 30px;
+        margin-right: 31px;
+        padding: 0;
+
         order: 1;
       }
 
@@ -160,68 +182,81 @@
 
     > .right {
       max-width: 436px;
-
-      padding: 35px;
-
-      color: #000000;
-      background: url("@/assets/images/logo/certo_c.png") no-repeat center/cover;
-      background-size: contain;
-      background-color: #ffc247;
-      border-radius: 42px;
+      width: 100%;
 
       @include minimize() {
-        width: 100%;
-        margin: 30px 0 80px 0;
+        padding-left: 16px;
+        padding-right: 16px;
+
         order: 2;
-      }
 
-      > .right-title {
-        display: block;
-        font-size: 23px;
-        font-weight: 700;
-      }
-      > .right-subtitle {
-        margin: 30px 0;
-        display: block;
-
-        font-size: 15px;
-      }
-
-      > .input-container {
-        width: 100%;
-        display: flex;
-
-        > .input-area {
-          height: 35px;
-          padding-left: 15px;
-          flex: 7;
-
-          font-size: 16px;
-          line-height: 35px;
-          outline: none;
-          resize: none;
-          border: none;
-          border-top-left-radius: 20px;
-          border-bottom-left-radius: 20px;
+        @include minimize() {
+          margin-top: 45px;
         }
-        > .input-container::placeholder {
-          font-family: "Inter", sans-serif;
-          color: #888;
-        }
-        > .submit-bt {
-          height: 35px;
-          padding: 7px 30px;
-          flex: 3;
+      }
+      > .yellow-container {
+        padding: 35px;
 
-          background-color: #02033b;
-          color: #ffffff;
-          font-size: 15px;
+        color: #02033b;
+        background: url("@/assets/images/logo/certo_c.png") no-repeat
+          center/cover;
+        background-size: contain;
+        background-color: #ffc247;
+        border-radius: 42px;
+
+        > .right-title {
+          display: block;
+          font-size: 23px;
           font-weight: 700;
+        }
+        > .right-subtitle {
+          margin: 30px 0;
+          display: block;
 
-          vertical-align: top;
-          border-top-right-radius: 20px;
-          border-bottom-right-radius: 20px;
-          cursor: pointer;
+          font-size: 15px;
+        }
+
+        > .input-container {
+          width: 100%;
+          display: flex;
+
+          > .input-area {
+            max-width: 254.8px;
+            height: 35px;
+            padding-left: 15px;
+            flex: 7;
+
+            font-size: 16px;
+            line-height: 35px;
+            outline: none;
+            resize: none;
+            border: none;
+            border-top-left-radius: 20px;
+            border-bottom-left-radius: 20px;
+
+            @include minimize() {
+              max-width: 179.8px;
+            }
+          }
+          > .input-container::placeholder {
+            font-family: "Inter", sans-serif;
+            color: #888;
+          }
+          > .submit-bt {
+            height: 35px;
+            padding: 7px 30px;
+            flex: 3;
+
+            background-color: #02033b;
+            color: #ffffff;
+            font-size: 15px;
+            font-weight: 700;
+
+            vertical-align: top;
+            border-top-right-radius: 20px;
+            border-bottom-right-radius: 20px;
+            cursor: pointer;
+          }
         }
       }
     }
@@ -232,8 +267,13 @@
     margin: 0 auto;
     padding-top: 188px;
 
-    @media (max-width: 1000px) {
+    @include minimize() {
+      padding-top: 21px;
+      padding-bottom: 47px;
+      padding-left: 29px;
+      padding-right: 38px;
     }
+
     > .text-apple {
       max-width: 539px;
       display: block;
